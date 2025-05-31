e7bf04196663fea5f63ff3e002192cf060e453fe
JONAH KYAGABA MUBUUKE <97627591+JONAHKYAGABA@users.noreply.github.com>
2025-05-31 12:46:12 +0300

    Update count_reviews.py

--- jobs/count_reviews.py
+++ jobs/count_reviews.py
@@ -1,25 +1,15 @@
 from mrjob.job import MRJob
 import json
-from typing import Iterator, Tuple
 
-class CountReviews(MRJob):
-    """Count number of reviews per parent_asin."""
+class CountReviewsPerProduct(MRJob):
+    def mapper(self, _, line):
+        data = json.loads(line)
+        asin = data.get("parent_asin")
+        if asin:
+            yield asin, 1
 
-    def mapper(self, _, line: str) -> Iterator[Tuple[str, int]]:
-        """Yield (parent_asin, 1) for each review."""
-        try:
-            rec = json.loads(line)
-            asin = rec.get("parent_asin")
-            if asin is not None:
-                yield asin, 1
-        except Exception:
-            return
-
-    def combiner(self, asin: str, counts: Iterator[int]) -> Iterator[Tuple[str, int]]:
-        yield asin, sum(counts)
-
-    def reducer(self, asin: str, counts: Iterator[int]) -> Iterator[Tuple[str, int]]:
+    def reducer(self, asin, counts):
         yield asin, sum(counts)
 
 if __name__ == "__main__":
-    CountReviews.run()
+    CountReviewsPerProduct.run()
